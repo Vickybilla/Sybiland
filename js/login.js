873ba837018ec2d9168319b3c3637f68b39b88f3
vickybilla <jimenezvictoria@hotmail.com>
2020-08-05 10:42:19 -0300

    Maquetado  de Sybiland para Le Wagon 1.0 /En Construcción: Falta agregar contenido ,intervenir formularios con js  y css y conectar algunas ref que aún no están listas.

---
+++ js/login.js
@@ -0,0 +1,14 @@
+  function Login(){ 
+  var done=0; 
+  var usr=document.login.inputEmail.value; 
+  var pass=document.login.inputPassword.value; 
+  if (usr=="jimena@gmail.com" && pass=="12345") { 
+  window.location="../html/Sybiland.html"; 
+  } 
+  if (usr=="victoria@gmail.com" && pass=="kryptonita") { 
+  window.location="../html/Sybiland.html"; 
+  } 
+  if (usr=="" && pass=="") { 
+    alert("Ingrese, nombre de usuario y contraseña correctos."); 
+  } 
+  } 
